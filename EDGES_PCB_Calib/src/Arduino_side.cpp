680d02be7cff0eb708cb8742c131ae067b3d30ba
titu85 <titu85@gmail.com>
2023-12-12 10:35:10 -0700

    Arduino serial testing
    Arduino serial port communication testing

---
+++ EDGES_PCB_Calib/src/Arduino_side.cpp
@@ -0,0 +1,50 @@
+// Include necessary libraries
+#include <Arduino.h>
+
+// Define LED pin
+const int ledPin = 32;
+const int conect = 34;
+
+void setup() {
+  // Start serial communication
+  Serial.begin(9600);
+
+  // Initialize LED pin as an output
+  pinMode(ledPin, OUTPUT);
+  pinMode(conect, OUTPUT);
+
+  while (!Serial) {
+    // Wait for the serial port to be opened
+     digitalWrite(conect, HIGH);
+     delay(1000);
+     digitalWrite(conect, LOW);
+     delay(1000);
+     //Serial.println("No Command received: ");
+  }
+
+  // Print a message to indicate that the Serial Monitor is active
+  Serial.println("Serial Monitor link is active!");
+
+}
+
+void loop() {
+  // Check if data is available to read from serial port
+  if (Serial.available() > 0) {
+    // Read the incoming command
+    String command = Serial.readStringUntil('\n');
+
+    // Process the command
+    if (command == "ON") {
+      digitalWrite(ledPin, HIGH);  // Turn the LED on
+      Serial.println("LED is ON");
+    } else if (command == "OFF") {
+      digitalWrite(ledPin, LOW);  // Turn the LED off
+      Serial.println("LED is OFF");
+    } else {
+      Serial.println("Invalid command");
+    }
+    } 
+    
+  // Add a short delay for stability
+  delay(1000);
+}
